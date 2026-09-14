c24684f9ffbaf6ddd1d81e82881a26de952c07be
Kathiresan K <kkathir1998@gmail.com>
2026-09-14 16:35:26 +0530

    Update bronze_flight.py

--- src/bronze_flight.py
+++ src/bronze_flight.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp,col
 source_path="/Volumes/workspace/flight_tracking/incoming"
 schema_path="/Volumes/workspace/flight_tracking/schema/bronze"
 checkpoint_path="/Volumes/workspace/flight_tracking/checkpoints/bronze"
@@ -10,7 +10,7 @@ bronze_df=spark.readStream.format("cloudFiles") \
   .option("recuedDataColumn", "rescued_data") \
   .load(source_path)\
   .withColumn("ingestion_time", current_timestamp())\
-  .withColumn("path",_metadata.file_path())
+  .withColumn("path",col("_metadata.file_path")
 bronze_query=bronze_df.writeStream \
   .format("delta") \
   .option("checkpointLocation", checkpoint_path) \

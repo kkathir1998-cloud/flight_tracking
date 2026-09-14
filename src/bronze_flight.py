a526122383d3c5cea7b800bf8f708efa24a6d678
Kathiresan K <kkathir1998@gmail.com>
2026-09-14 16:31:23 +0530

    Update bronze_flight.py

--- src/bronze_flight.py
+++ src/bronze_flight.py
@@ -1,2 +1,19 @@
-print("hello")
+source_path="/Volumes/workspace/flight_tracking/incoming"
+schema_path="/Volumes/workspace/flight_tracking/schema/bronze"
+checkpoint_path="/Volumes/workspace/flight_tracking/checkpoints/bronze"
 
+bronze_df=spark.readStream.format("cloudFiles") \
+  .option("cloudFiles.format", "json") \
+  .option("cloudFiles.schemaLocation", schema_path) \
+  .option("cloudFiles.schemaEvolutionMode", "rescue") \
+  .option("recuedDataColumn", "rescued_data") \
+  .load(source_path)\
+  .withColumn("ingestion_time", current_timestamp())\
+  .withColumn("path",_metadata.file_path())
+bronze_query=bronze_df.writeStream \
+  .format("delta") \
+  .option("checkpointLocation", checkpoint_path) \
+  .trigger(availableNows=True) \
+  .toTable(workspace.flight_tracking.bronze)
+
+  
